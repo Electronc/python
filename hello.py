0ad92697413da528d248f21d00e317d3871cdf30
Electronc <electroncmanmine@gmail.com>
2022-02-16 21:57:33 +0100

    zmiany dodatkowe w 03

--- hello.py
+++ hello.py
@@ -1,2 +1,9 @@
-print("hello world!")
-print("Dodałem coś nowego!")
+def policz(a,b):
+    return (a*b)*(a-b)
+
+
+if __name__ == "__main__":
+    a=2
+    b=3
+    print("hello world!")
+    print('Wynik działania: '+str(policz(a,b)))
