41411efdb0f21255a2c11a2dea74a4982d932f6c
Electronc <electroncmanmine@gmail.com>
2022-02-16 20:48:01 +0100

    zmieniono dodawanie na mnożenia w funkcji policz

--- hello.py
+++ hello.py
@@ -1,9 +1,9 @@
 def policz(a,b):
-    return a+b
+    return a*b
 
 
 if __name__ == "__main__":
     a=2
     b=3
     print("hello world!")
-    print('Wynik dodawania '+str(a)+" + "+str(b)+" = " +str(policz(a,b)))
+    print('Wynik mnożenia '+str(a)+" * "+str(b)+" = " +str(policz(a,b)))
